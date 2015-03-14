bc55991d13cf7b4f9b31567393e9ee93244a612c
suren.anem@gmail.com <suren123>
2015-03-14 11:59:38 -0400

    Initial import

---
+++ AOP/src/main/java/com/chennaisoft/AppMain.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.chennaisoft;
+
+import org.chennaisoft.service.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author surendra
+ * 
+ */
+public class AppMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+		
+		System.out.println(shapeService.getCircle().getName());
+
+	}
+
+}

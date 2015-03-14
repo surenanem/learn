dcd6f3212561357d52aa0ff8eef164e307146d03
suren.anem@gmail.com <suren123>
2015-03-14 19:04:43 -0400

    point cut examples included

--- AOP/src/main/java/com/chennaisoft/aspect/LoggingAspect.java
+++ AOP/src/main/java/com/chennaisoft/aspect/LoggingAspect.java
@@ -6,22 +6,43 @@ package com.chennaisoft.aspect;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * @author surendra
- *
+ * 
  */
-
 
 @Aspect
 public class LoggingAspect {
-	
+
 	private static Logger log = Logger.getLogger(LoggingAspect.class);
 
-	@Before("execution(public String getName())")
-	public void loggingAdvice() {
-		//System.out.println("Advice run. Get Method Called...");
+	@Before("forCircleMethods()")
+	public void loggingAdvice1() {
 		log.debug("Advice run. Get Method Called...");
+	}
+
+	@Before("allCircleMethods()")
+	public void loggingAdvice2() {
+		log.debug("Advice run. allCircleMethods...");
+	}
+
+	// @Before("allGetters()")
+	public void secondAdvice() {
+		log.debug("Second Advice Executed....");
+	}
+
+	@Pointcut("execution(* get*())")
+	public void allGetters() {
+	}
+
+	@Pointcut("execution(* org.chennaisoft.model.Circle.getName())")
+	public void forCircleMethods() {
+	}
+
+	@Pointcut("within(org.chennaisoft.model.Circle)")
+	public void allCircleMethods() {
 	}
 
 }
--- AOP/src/main/java/org/chennaisoft/model/Circle.java
+++ AOP/src/main/java/org/chennaisoft/model/Circle.java
@@ -4,14 +4,12 @@ public class Circle {
 	private String name;
 
 	public String getName() {
-		return getMyName();
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	protected String getMyName() {
-		return "surendra";
-	}
+	
 }
